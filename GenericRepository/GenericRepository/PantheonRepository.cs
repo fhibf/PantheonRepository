9b18d51da39dc99093727c91f7b420125fdfc50d
Fernando Henrique Inocêncio Borba Ferreira <ferhenriquef@live.com>
2013-03-11 15:07:49 -0300

    First Commit

---
+++ GenericRepository/GenericRepository/PantheonRepository.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Objects;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepository
+{
+    public class PantheonRepository<TEntity> 
+            where TEntity : class
+    {
+        private enum RepositoryAction
+        {
+            None,
+            Updating,
+            Inserting,
+            Deleting,
+            Searching
+        }
+
+        private RepositoryAction _currentAction;
+
+        /// <summary>
+        /// Create new context do manipulate data.
+        /// </summary>
+        /// <returns>Return new instance of the context.</returns>
+        public DbContext CreateContext()
+        {
+            var context = new ContextoDados();
+
+            return context;
+        }
+
+        /// <summary>
+        /// Save data into database.
+        /// </summary>
+        /// <param name="entity">Entity to save.</param>
+        /// <returns>Entity saved.</returns>
+        public virtual TEntity Save(TEntity entity)
+        {
+            return Save(entity, false);
+        }
+
+        /// <summary>
+        /// Save data into database.
+        /// </summary>
+        /// <param name="entities">Entities to save.</param>
+        /// <returns>Entity saved.</returns>
+        public virtual TEntity Save(IEnumerable<TEntity> entities)
+        {
+            return Save(entities, false).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Save data into database.
+        /// </summary>
+        /// <param name="entity">Entity to save.</param>c
+        /// <param name="saveNestedProperties">Indicated repopsitory needs update nested properties</param>
+        /// <returns>Entity saved.</returns>
+        public virtual TEntity Save(TEntity entity, bool saveNestedProperties)
+        {
+            return Save(new TEntity[] { entity }, saveNestedProperties).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Save data into database.
+        /// </summary>
+        /// <param name="entities">Entities to save.</param>c
+        /// <param name="saveNestedProperties">Indicated repopsitory needs update nested properties</param>
+        /// <returns>Entity saved.</returns>
+        public virtual IEnumerable<TEntity> Save(IEnumerable<TEntity> entities, bool saveNestedProperties)
+        {
+            using (var context = CreateContext())
+            {
+                foreach (var entity in entities)
+                {
+                    // Identity primary key value.
+                    int primaryKeyValue = IdentityPrimaryKeyValue(entity, context);
+
+                    // Identify current action
+                    this._currentAction = primaryKeyValue == 0 ? RepositoryAction.Inserting : RepositoryAction.Updating;
+
+                    // Save entity to database.
+                    SaveEntity(entity, context, primaryKeyValue);
+
+                    // Update nested properties
+                    UpdateNestedProperties(entity, context, saveNestedProperties);
+                }
+
+                // Commit changes to database
+                context.SaveChanges();
+            }
+
+            return entities;
+        }
+
+        private void UpdateNestedProperties(TEntity entity, DbContext context, bool saveNestedProperties)
+        {
+            Type typeGeneric = entity.GetType();
+
+            foreach (var property in typeGeneric.GetProperties())
+            {
+                if (IsEntityProperty(property))
+                {
+                    if (property.PropertyType.IsArray)
+                    {
+                        IEnumerable<object> itensOfArray = (IEnumerable<object>)property.GetValue(entity);
+
+                        if (itensOfArray != null)
+                        {
+                            foreach (var itemOfArray in itensOfArray)
+                            {
+                                SetStateOfEntity(itemOfArray, context, saveNestedProperties, entity);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var nestedPropertyToUpdate = property.GetValue(entity);
+
+                        if (nestedPropertyToUpdate != null)
+                        {
+                            SetStateOfEntity(nestedPropertyToUpdate, context, saveNestedProperties, entity);
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsEntityProperty(PropertyInfo property)
+        {
+            return property.PropertyType.IsClass && property.PropertyType != typeof(string);
+        }
+
+        private void SetNestedPropertiesStatus(object entity, DbContext context, EntityState state, object parent)
+        {
+            Type typeGeneric = entity.GetType();
+
+            foreach (var property in typeGeneric.GetProperties())
+            {
+                if (IsEntityProperty(property))
+                {
+                    if (property.PropertyType.IsArray)
+                    {
+                        IEnumerable<object> itensOfArray = (IEnumerable<object>)property.GetValue(entity);
+
+                        if (itensOfArray != null)
+                        {
+                            foreach (PropertyInfo itemOfArray in itensOfArray)
+                            {
+                                var value = itemOfArray.GetValue(entity);
+
+                                if (value != parent)
+                                {
+                                    context.Entry(value).State = state;
+
+                                    SetNestedPropertiesStatus(value, context, state, entity);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var nestedPropertyToUpdate = property.GetValue(entity);
+
+                        if (nestedPropertyToUpdate != null)
+                        {
+                            if (nestedPropertyToUpdate != parent)
+                            {
+                                context.Entry(nestedPropertyToUpdate).State = state;
+
+                                SetNestedPropertiesStatus(nestedPropertyToUpdate, context, state, entity);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetStateOfEntity(object entity, DbContext context, bool saveNestedProperties, object parent)
+        {
+            EntityState targetState = EntityState.Added;
+
+            // Capture ID value from nested property
+            int nestedPropertyPrimaryKeyValue = IdentityPrimaryKeyValue(entity, context);
+
+            // Actions during insert
+            if (this._currentAction == RepositoryAction.Inserting)
+            {
+                // Verify if property needs to be added or stay as unchanged
+                if (nestedPropertyPrimaryKeyValue == 0)
+                    targetState = saveNestedProperties ? EntityState.Added : EntityState.Unchanged;
+                else
+                    targetState = EntityState.Unchanged;
+            }
+            // Actions during update
+            else if (this._currentAction == RepositoryAction.Updating)
+            {
+                // If nestedPropertyPrimaryKeyValue equals zero, so this instance must be added
+                if (nestedPropertyPrimaryKeyValue == 0)
+                    targetState = saveNestedProperties ? EntityState.Added : EntityState.Unchanged;
+                else
+                    targetState = saveNestedProperties ? EntityState.Modified : EntityState.Unchanged;
+            }
+
+            context.Entry(entity).State = targetState;
+
+            if (targetState == EntityState.Unchanged)
+                SetNestedPropertiesStatus(entity, context, targetState, parent);
+        }
+
+        private void SaveEntity(TEntity entity, DbContext context, int currentPrimaryKeyValue)
+        {
+            if (currentPrimaryKeyValue == 0)
+                context.Set<TEntity>().Add(entity);
+            else
+                context.Entry(entity).State = EntityState.Modified;
+        }
+
+        private int IdentityPrimaryKeyValue(object entity, DbContext context)
+        {
+            int returnValue = 0;
+            var primaryKeyProperties = GetKeyNames(context);
+
+            Type typeGeneric = entity.GetType();
+
+            var primaryKeyProperty = typeGeneric.GetProperties().Where(p => string.Compare(p.Name, primaryKeyProperties[0], true) == 0).FirstOrDefault();
+
+            if (primaryKeyProperty != null)
+            {
+                returnValue = (int)primaryKeyProperty.GetValue(entity);
+            }
+
+            return returnValue;
+        }
+
+        private string[] GetKeyNames(DbContext context)
+        {
+            ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
+            ObjectSet<TEntity> set = objectContext.CreateObjectSet<TEntity>();
+            IEnumerable<string> keyNames = set.EntitySet.ElementType
+                                                        .KeyMembers
+                                                        .Select(k => k.Name);
+
+            return keyNames.ToArray();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            this._currentAction = RepositoryAction.Deleting;
+
+            using (var context = CreateContext())
+            {
+                context.Set<TEntity>().Attach(entity);
+                context.Set<TEntity>().Remove(entity);
+
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<TEntity> Query(Func<TEntity, bool> query)
+        {
+            this._currentAction = RepositoryAction.Searching;
+
+            return Query(query, null);
+        }
+
+        public IEnumerable<TEntity> GetAll(params string[] includeProperties)
+        {
+            this._currentAction = RepositoryAction.Searching;
+
+            IEnumerable<TEntity> result = null;
+
+            // Create database context
+            using (var context = CreateContext())
+            {
+                // Create instance of DbSet
+                var dbSet = context.Set<TEntity>();
+
+                DbQuery<TEntity> currentQuery = null;
+
+                // Verify if the user wants to use includes
+                if (includeProperties != null && includeProperties.Count() > 0)
+                {
+                    foreach (string propertyInclude in includeProperties)
+                    {
+                        // Add includes to query...
+                        if (currentQuery == null)
+                            currentQuery = dbSet.Include(propertyInclude);
+                        else
+                            currentQuery.Include(propertyInclude);
+                    }
+
+                    result = currentQuery;
+                }
+                else
+                {
+                    result = dbSet;
+                }
+
+                result = result.ToList();
+            }
+
+            return result;
+        }
+
+        public IEnumerable<TEntity> Query(Func<TEntity, bool> query, params string[] includeProperties)
+        {
+            this._currentAction = RepositoryAction.Searching;
+
+            IEnumerable<TEntity> result = null;
+
+            // Create database context
+            using (var context = CreateContext())
+            {
+                // Create instance of DbSet
+                var dbSet = context.Set<TEntity>();
+
+                DbQuery<TEntity> currentQuery = null;
+
+                // Verify if the user wants to use includes
+                if (includeProperties != null && includeProperties.Count() > 0)
+                {
+                    foreach (string propertyInclude in includeProperties)
+                    {
+                        // Add includes to query...
+                        if (currentQuery == null)
+                            currentQuery = dbSet.Include(propertyInclude);
+                        else
+                            currentQuery.Include(propertyInclude);
+                    }
+                }
+
+                // Execute query
+                if (currentQuery == null)
+                    result = dbSet.Where(query);
+                else
+                    result = currentQuery.Where(query);
+
+                result = result.ToList();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GenericRepository/Playground/Program.cs
@@ -0,0 +1,157 @@
+﻿using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {            
+                // Criar novos países
+                CriarNovosPaises();
+
+                // Criar novos produtos
+                CriarNovosProdutos();
+
+                // Criar novos clientes
+                CriarNovosClientes();
+
+                // Criar novos pedidos
+                CriarNovosPedidos();
+
+            }
+        }
+
+        private static void CriarNovosPedidos()
+        {
+            var repProdutos = new PantheonRepository<Produto>();
+            var repClientes = new PantheonRepository<Customer>();
+
+            var produtos = repProdutos.GetAll().ToArray();
+            var clientes = repClientes.GetAll("EnderecoAtual").ToArray();
+
+            var listOfPedidos = new List<SalesOrder>(1);
+            for (int i = 0; i < 50; i++)
+            {
+                Random random = new Random(DateTime.Now.Millisecond);
+
+                SalesOrder pedido = new SalesOrder();
+                pedido.Cliente = clientes[random.Next(0, clientes.Length)];                
+
+                int quantidadeItens = random.Next(0, 20);
+
+                for (int j = 0; j < quantidadeItens; j++)
+                {
+                    SalesOrderItem itemVenda = new SalesOrderItem();
+
+                    itemVenda.Produto = produtos[random.Next(0, produtos.Length)];
+                    itemVenda.Quantidade = random.Next(0, 5);
+                    itemVenda.ValorVenda = itemVenda.Produto.ValorVenda;
+                    itemVenda.PedidoVenda = pedido;
+                    
+                    pedido.Items.Add(itemVenda);
+                }
+                
+                System.Threading.Thread.Sleep(1);
+
+                listOfPedidos.Add(pedido);
+            }
+
+            var repPedidos = new PantheonRepository<SalesOrder>();
+            repPedidos.Save(listOfPedidos, true);
+
+            repPedidos.Save(listOfPedidos);
+        }
+
+        private static void CriarNovosClientes()
+        {
+            var repPaises= new PantheonRepository<Country>();
+            
+            var paises = repPaises.GetAll().ToArray();
+
+            var listOfClientes = new List<Customer>();
+            for (int i = 0; i < 50; i++)
+            {
+                var randomGenerator = new Random(DateTime.Now.Millisecond);
+
+                Customer novoCliente = new Customer();
+
+                novoCliente.Nome = "Cliente " + (i + 1).ToString();
+                novoCliente.EnderecoAtual = new Address();
+                novoCliente.EnderecoAtual.Bairro = "Bairro";
+                novoCliente.EnderecoAtual.Cep = "CEP";
+                novoCliente.EnderecoAtual.Logradouro = "Logradouro";
+                novoCliente.EnderecoAtual.Numero = "Numero";
+                novoCliente.EnderecoAtual.Pais = paises[randomGenerator.Next(0, paises.Count())];
+                
+                listOfClientes.Add(novoCliente);
+
+                System.Threading.Thread.Sleep(1);
+            }
+
+            var repClientes = new PantheonRepository<Customer>();
+
+            repClientes.Save(listOfClientes, true);
+
+            repClientes.Save(listOfClientes);
+        }
+
+        private static void CriarNovosProdutos()
+        {
+            var listOfProdutos = new List<Produto>(100);
+            for (int i = 0; i < 100; i++)
+            {
+                var randomGenerator = new Random(DateTime.Now.Millisecond);
+
+                Produto produto = new Produto();
+                produto.Nome = "Produto " + (i + 1).ToString();
+                produto.ValorCompra = (decimal)(randomGenerator.Next(1000) + randomGenerator.NextDouble());
+                produto.ValorVenda = produto.ValorCompra * 1.15M;
+
+                listOfProdutos.Add(produto);
+
+                System.Threading.Thread.Sleep(1);
+            }
+
+            var rep = new PantheonRepository<Produto>();
+
+            rep.Save(listOfProdutos);
+
+            rep.Save(listOfProdutos);
+        }
+
+        private static void CriarNovosPaises()
+        {
+            var rep = new PantheonRepository<Country>();
+
+            var query = rep.GetAll();
+
+            if (query.Count() == 0)
+            {
+                Country paisBrasil = new Country() { Continente = Continent.America, Nome = "Brasil" };
+                Country paisChina = new Country() { Continente = Continent.Asia, Nome = "China" };
+                Country paisEUA = new Country() { Continente = Continent.America, Nome = "EUA" };
+                Country paisPortugal = new Country() { Continente = Continent.Europa, Nome = "Portugal" };
+                Country paisJapao = new Country() { Continente = Continent.Asia, Nome = "Japão" };
+                Country paisEgito = new Country() { Continente = Continent.Africa, Nome = "Egito" };
+                Country paisInglaterra = new Country() { Continente = Continent.Europa, Nome = "Inglaterra" };
+                
+                rep.Save(new Country[] {
+                                    paisBrasil,
+                                    paisChina,
+                                    paisEUA,
+                                    paisPortugal,
+                                    paisJapao,
+                                    paisEgito,
+                                    paisInglaterra
+                                });
+            }
+        }
+    }
+}
